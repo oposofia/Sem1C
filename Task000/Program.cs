3fd283feb144bceb94185672a718061ee0bac0d6
OpoSofia <sko88@bk.ru>
2023-09-18 22:26:04 +0400

    Task0 square

--- Task000/Program.cs
+++ Task000/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Нахождение квадрата числа
+// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.WriteLine("Введите число");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+int square = NumberA * NumberA;
+Console.WriteLine(NumberA + " -> " + square);
+Console.WriteLine($"{NumberA} -> {square}");
+
