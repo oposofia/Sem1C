a4ecda52623a03203654be067de6dd17d271b377
OpoSofia <sko88@bk.ru>
2023-09-22 22:36:37 +0400

    Task 002 Homework

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -6,15 +6,15 @@
 // a = 9, b = -3 -> да
 // a = -3 b = 9 -> нет
 Console.WriteLine("Введите первое число");
-int number1 = Convert.ToInt32(Console.Readline());
+int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
-int number2 = Convert.ToInt32(Console.Readline());
+int number2 = Convert.ToInt32(Console.ReadLine());
 if (number1 == number2*number2)
 {
-    Console.WriteLine($"{number2} является квадратом {number1}");
+    Console.WriteLine($"{number1} является квадратом {number2}");
 }
 else 
 {
-    Console.WriteLine($"{number2} не является квадратом {number1}");
+    Console.WriteLine($"{number1} не является квадратом {number2}");
 }
 
